c6d779d10a4faee4c7902dc3ef9e5c4d227b9757
HatsuneMiku <999Hatsune@gmail.com>
2012-11-09 11:33:48 +0900

    add ssxcopy.py (skeleton)

---
+++ ssxcopy.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''ssxcopy
+'''
+
+import sys, os
+import time
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def ssxcopy(base):
+  print base
+
+if __name__ == '__main__':
+  base = BASE_DIR
+  ssxcopy(base)
