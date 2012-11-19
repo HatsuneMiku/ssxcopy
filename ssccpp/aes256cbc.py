51b7468f5bb6a4ee01d5d14f70b77410c4f93935
HatsuneMiku <999Hatsune@gmail.com>
2012-11-19 14:51:11 +0900

    modify aes256cbc.py (Salted__ progressbar chunksize padding)

--- ssccpp/aes256cbc.py
+++ ssccpp/aes256cbc.py
@@ -14,20 +14,25 @@ from Crypto.Cipher import AES
 import bz2
 
 class AES256CBC(object):
-  IV_LENGTH = 16
+  LENGTH_SALT = 8
   DEFAULT_CHUNKSIZE = 16 * 4096
-  DEFAULT_PROGRESS_MAX = 1000000
 
   @staticmethod
-  def getiv(length=IV_LENGTH):
+  def get_random_bytes(length=LENGTH_SALT):
     return ''.join(chr(random.randint(0, 0xFF)) for _ in range(length))
 
-  def __init__(self, key, chunksize=DEFAULT_CHUNKSIZE):
-    self.key = key
-    self.key32 = hashlib.sha256(key)
+  @staticmethod
+  def get_key_iv(passwd, salt):
+    h = [''] * 3
+    for i in range(len(h)):
+      h[i] = hashlib.md5((h[i - 1] if i else '') + passwd + salt).digest()
+    return h[0] + h[1], h[2]
+
+  def __init__(self, passwd, chunksize=DEFAULT_CHUNKSIZE):
+    self.passwd = passwd
     self.chunksize = chunksize
 
-  def progress(self, mode, name, maxval=DEFAULT_PROGRESS_MAX):
+  def progress(self, mode, name, maxval):
     sname = '%s...%s' % (name[:9], name[-9:]) if len(name) >= 21 else name
     self.widgets = ['%s %s: ' % (mode, sname),
       pgbar.Bar(marker=pgbar.RotatingMarker()), ' ', pgbar.Percentage(),
@@ -36,23 +41,27 @@ class AES256CBC(object):
 
   def file_encrypt(self, infile, outfile=None):
     plainmd5, encryptmd5 = hashlib.md5(), hashlib.md5()
-    if not outfile: outfile = infile + '.enc'
+    if outfile is None: outfile = infile + '.enc'
     filesize = os.path.getsize(infile)
-    iv = AES256CBC.getiv()
-    self.pbar = self.progress('e', infile)
-    encryptor = AES.new(self.key32.digest(), AES.MODE_CBC, iv)
+    self.pbar = self.progress('e', infile, filesize)
     ifp = open(infile, 'rb')
     ofp = open(outfile, 'wb')
-    ofp.write(struct.pack('>Q', filesize))
-    ofp.write(iv)
-    c = 0
+    ofp.write('Salted__')
+    salt = AES256CBC.get_random_bytes()
+    ofp.write(salt)
+    key32, iv16 = AES256CBC.get_key_iv(self.passwd, salt)
+    encryptor = AES.new(key32, AES.MODE_CBC, iv16)
+    c, d = 0, False
     while True:
       chunk = ifp.read(self.chunksize)
-      if len(chunk) == 0: break
-      elif len(chunk) % 16 != 0: chunk += ' ' * (16 - len(chunk) % 16)
+      if len(chunk) == 0 or len(chunk) % 16 != 0:
+        d = True
+        pad = 16 - (len(chunk) % 16) # pad should be never 0,
+        chunk += chr(pad) * pad # so remove them later 1-16
       ofp.write(encryptor.encrypt(chunk))
       c += len(chunk)
-      self.pbar.update(c * AES256CBC.DEFAULT_PROGRESS_MAX / filesize)
+      self.pbar.update(c if c <= filesize else filesize)
+      if d: break
     ofp.close()
     ifp.close()
     self.pbar.finish()
@@ -60,31 +69,46 @@ class AES256CBC(object):
 
   def file_decrypt(self, infile, outfile=None):
     plainmd5, decryptmd5 = hashlib.md5(), hashlib.md5()
-    if not outfile: outfile = os.path.splitext(infile)[0]
+    if outfile is None: outfile = os.path.splitext(infile)[0]
+    filesize = os.path.getsize(infile)
+    self.pbar = self.progress('d', infile, filesize)
     ifp = open(infile, 'rb')
-    orgsize = struct.unpack('>Q', ifp.read(struct.calcsize('Q')))[0]
-    iv = ifp.read(AES256CBC.IV_LENGTH)
-    self.pbar = self.progress('d', infile)
-    decryptor = AES.new(self.key32.digest(), AES.MODE_CBC, iv)
-    ofp = open(outfile, 'wb')
-    c = 0
-    while True:
-      chunk = ifp.read(self.chunksize)
-      if len(chunk) == 0: break
-      ofp.write(decryptor.decrypt(chunk))
-      c += len(chunk)
-      self.pbar.update(c * AES256CBC.DEFAULT_PROGRESS_MAX / orgsize)
-    ofp.truncate(orgsize)
-    ofp.close()
+    if ifp.read(AES256CBC.LENGTH_SALT) != 'Salted__':
+      sys.stderr.write('header Salted__ is not found\n')
+    else:
+      salt = ifp.read(AES256CBC.LENGTH_SALT)
+      key32, iv16 = AES256CBC.get_key_iv(self.passwd, salt)
+      decryptor = AES.new(key32, AES.MODE_CBC, iv16)
+      ofp = open(outfile, 'wb')
+      c, d = AES256CBC.LENGTH_SALT * 2, False
+      while True:
+        chunk = ifp.read(self.chunksize)
+        if len(chunk) == 0:
+          sys.stderr.write('unexpected EOF is found (%d)\n' % (c))
+          break
+        dat = decryptor.decrypt(chunk)
+        c += len(chunk)
+        if c >= filesize:
+          d = True
+          pad = ord(dat[-1])
+          if 1 <= pad <= 16: ofp.write(dat[:-pad])
+          else:
+            sys.stderr.write('padding may be incorrect\n')
+            ofp.write(dat)
+        else:
+          ofp.write(dat)
+        self.pbar.update(c if c <= filesize else filesize)
+        if d: break
+      ofp.close()
     ifp.close()
     self.pbar.finish()
     return (plainmd5, decryptmd5)
 
   def file_compress(self, infile, outfile=None):
     plainmd5, compressmd5 = hashlib.md5(), hashlib.md5()
-    if not outfile: outfile = infile + '.bz2'
+    if outfile is None: outfile = infile + '.bz2'
     filesize = os.path.getsize(infile)
-    self.pbar = self.progress('c', infile)
+    self.pbar = self.progress('c', infile, filesize)
     compressor = bz2.BZ2Compressor(9)
     ifp = open(infile, 'rb')
     ofp = open(outfile, 'wb')
@@ -94,7 +118,7 @@ class AES256CBC(object):
       if len(chunk) == 0: break
       ofp.write(compressor.compress(chunk))
       c += len(chunk)
-      self.pbar.update(c * AES256CBC.DEFAULT_PROGRESS_MAX / filesize)
+      self.pbar.update(c)
     ofp.write(compressor.flush())
     ofp.close()
     ifp.close()
@@ -103,9 +127,9 @@ class AES256CBC(object):
 
   def file_decompress(self, infile, outfile=None):
     plainmd5, decompressmd5 = hashlib.md5(), hashlib.md5()
-    if not outfile: outfile = os.path.splitext(infile)[0]
+    if outfile is None: outfile = os.path.splitext(infile)[0]
     filesize = os.path.getsize(infile)
-    self.pbar = self.progress('x', infile)
+    self.pbar = self.progress('x', infile, filesize)
     decompressor = bz2.BZ2Decompressor()
     ifp = open(infile, 'rb')
     ofp = open(outfile, 'wb')
@@ -115,7 +139,7 @@ class AES256CBC(object):
       if len(chunk) == 0: break
       ofp.write(decompressor.decompress(chunk))
       c += len(chunk)
-      self.pbar.update(c * AES256CBC.DEFAULT_PROGRESS_MAX / filesize)
+      self.pbar.update(c)
     ofp.close()
     ifp.close()
     self.pbar.finish()
@@ -126,41 +150,43 @@ class AES256CBC(object):
 
   def stream_compress_encrypt(self):
     plainmd5, c_md5, e_md5 = hashlib.md5(), hashlib.md5(), hashlib.md5()
-    self.pbar = self.progress('c', infile)
+    filesize = os.path.getsize(infile)
+    self.pbar = self.progress('c', infile, filesize)
     self.pbar.finish()
     return (plainmd5, c_md5, e_md5)
 
   def stream_decrypt_decompress(self):
     plainmd5, d_md5, x_md5 = hashlib.md5(), hashlib.md5(), hashlib.md5()
-    self.pbar = self.progress('x', infile)
+    filesize = os.path.getsize(infile)
+    self.pbar = self.progress('x', infile, filesize)
     self.pbar.finish()
     return (plainmd5, d_md5, x_md5)
 
 if __name__ == '__main__':
-  iv = AES256CBC.getiv()
-  key32 = hashlib.sha256('password')
-  print key32.hexdigest()
+  salt = AES256CBC.get_random_bytes()
+  key32, iv16 = AES256CBC.get_key_iv('password', salt)
   plaintext = 'c' * 16 + 'a' * 512 + 'b' * 1024 * 1024
-  print len(plaintext) - 16 - 512            # 1048576
-  encryptor = AES.new(key32.digest(), AES.MODE_CBC, iv)
+  print len(plaintext) - 16 - 512       # 1048576
+  encryptor = AES.new(key32, AES.MODE_CBC, iv16)
   ciphertext = encryptor.encrypt(plaintext)
   print len(ciphertext) - 16 - 512      # 1048576
-  decryptor = AES.new(key32.digest(), AES.MODE_CBC, iv)
+  decryptor = AES.new(key32, AES.MODE_CBC, iv16)
   txt = decryptor.decrypt(ciphertext)
   print len(txt) - 16 - 512             # 1048576
   print 'compare: %s' % (txt == plaintext)
 
-  plainfile = './test.mp3' # './aes256cbc.py'
+  # plainfile = './aes256cbc.py'
+  plainfile = './privatedata/test.mp3' # 60228786 = (14704:1202), 1202 = (75:2)
   a256c = AES256CBC('key')
 
-  cipherfile = '%s_test1.enc' % plainfile
   a256c.file_encrypt(plainfile)
+  cipherfile = '%s_test1.enc' % plainfile
   if os.path.exists(cipherfile): os.remove(cipherfile)
   os.rename('%s.enc' % plainfile, cipherfile)
   a256c.file_decrypt(cipherfile)
 
-  compressedfile = '%s_test2.bz2' % plainfile
   a256c.file_compress(plainfile)
+  compressedfile = '%s_test2.bz2' % plainfile
   if os.path.exists(compressedfile): os.remove(compressedfile)
   os.rename('%s.bz2' % plainfile, compressedfile)
   a256c.file_decompress(compressedfile)
